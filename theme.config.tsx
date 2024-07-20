3d4b151af99b17826f83077985bb5fa81159aa35
Benjamin Lu <benceruleanlu@proton.me>
2024-07-20 04:58:19 -0400

    feat: add teacher page

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,7 @@
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span style={{ fontWeight: 500, fontSize: '1.35em' }}>ISELP Programming</span>,
+  logo: <span style={{ fontWeight: 500, fontSize: '1.15em' }}>ISELP Programming</span>,
   project: {
     link: 'https://github.com/benceruleanlu/iselp2',
   },
